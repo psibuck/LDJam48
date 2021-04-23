815308b50abed6f5e098ec17e1e62f0f7f2ea702
psibuck <psibuck@gmail.com>
2021-04-23 08:51:12 +0100

    Initial commit of gitignore and project files

---
+++ LudumDare48/Source/LudumDare48/LudumDare48.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LudumDare48.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LudumDare48, "LudumDare48" );
---
+++ LudumDare48/Source/LudumDare48/LudumDare48GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LudumDare48GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUDUMDARE48_API ALudumDare48GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
