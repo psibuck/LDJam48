a2dfd882bfc61a637f79450efc4e864b7ead8363
psibuck <psibuck@gmail.com>
2021-04-24 21:22:38 +0100

    All off the ship madness, you can now fly a ship around if you drop it in

---
+++ LudumDare48/Source/LudumDare48/Core/PlayerShip.h
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+
+#include "PlayerShip.generated.h"
+
+UENUM()
+enum E_Movement
+{
+	RotateLeft = 1 << 0,
+	RotateRight = 1 << 1,
+	ThrustUp = 1 << 2,
+	ThrustDown = 1 << 3
+};
+
+DECLARE_DELEGATE_OneParam(FRotationInputComponent, const E_Movement);
+
+UCLASS()
+class LUDUMDARE48_API APlayerShip : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	APlayerShip();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Deep Space | Initial Settings")
+	float RotationalSpeed{ 5.0f };
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "Deep Space | Initial Settings")
+	float RocketPower{ 0.2f };
+
+private:
+	bool IsMovementFlagSet(const E_Movement flag) const;
+
+	UFUNCTION()
+	void SetMovementFlag(const E_Movement flag);
+	
+	UFUNCTION()
+	void ClearMovementFlag(const E_Movement flag);
+
+	float m_thrustLevel{ 0.0f };
+
+	int8 m_rotationControl{ 0 };
+};
