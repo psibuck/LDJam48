1c91087068cf595b760cf7c0f3fefcde23709807
Matthew Taylor <12675057+Twidge@users.noreply.github.com>
2021-04-24 23:03:23 +0100

    Planet construction and ProcGen test code

---
+++ LudumDare48/Source/LudumDare48/Planet.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Planet.h"
+
+// Sets default values
+APlanet::APlanet()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APlanet::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlanet::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ LudumDare48/Source/LudumDare48/PlanetProcGen.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class LUDUMDARE48_API PlanetProcGen
+{
+public:
+	PlanetProcGen();
+	~PlanetProcGen();
+};
